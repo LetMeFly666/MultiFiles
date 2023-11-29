0b1c719b14d32e7490382a8da55c39f1b2d6f042
LetMeFly666 <814114971@qq.com>
2023-11-29 23:47:26 +0800

    chore: MultiFilesMerger
    未运行验证

--- MultiFiles/dllmain.cpp
+++ MultiFiles/dllmain.cpp
@@ -17,3 +17,20 @@ BOOL APIENTRY DllMain( HMODULE hModule,
     return TRUE;
 }
 
+void MultiFilesMerger::merge(vector<string>& toGenFiles, string toGenFileName = "merged.tffs", string infoFileName = "merged-info.json") {
+    int totalBytes = 0;
+    ofstream ostrTFFS(toGenFileName, ios::out | ios::binary);
+    ofstream ostrInfo(infoFileName, ios::out);
+    for (string& thisFile : toGenFiles) {
+        ifstream istr(thisFile, ios::in | ios::binary);
+        string data;
+        istr >> data;
+        ostrTFFS << data;
+        ostrInfo << thisFile << '|' << totalBytes << '|' << data.size() << endl;  // 文件名|开始位置|文件长度\n
+        totalBytes += data.size();
+        istr.close();
+    }
+    ostrTFFS.close();
+    ostrInfo.close();
+}
+
--- MultiFiles/framework.h
+++ MultiFiles/framework.h
@@ -3,3 +3,21 @@
 #define WIN32_LEAN_AND_MEAN             // 从 Windows 头文件中排除极少使用的内容
 // Windows 头文件
 #include <windows.h>
+#include <iostream>
+#include <fstream>
+#include <cstring>
+#include <vector>
+using namespace std;
+
+extern "C" __declspec(dllexport) class MultiFilesMerger {
+public:
+	/* 将toGenFiles中的所有文件压缩合并到toGenFileName中 */
+	void __declspec(dllexport) merge(vector<string>& toGenFiles, string toGenFileName = "merged.tffs", string infoFileName = "merged-info.json");
+};
+
+extern "C" __declspec(dllexport) class MultiFilesReader {
+public:
+	__declspec(dllexport) MultiFilesReader(string mergedFileName = "merged.ttfs", string infoFileName = "merged-info.json");
+	/* 读取一个小文件 */
+	string __declspec(dllexport) read(string toReadFileName);
+};
