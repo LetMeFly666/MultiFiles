a3f217aad6b1233f9641770dbf95b025abbd8011
LetMeFly666 <814114971@qq.com>
2023-11-30 16:58:13 +0800

    chore: not ignore small files, use several files as sample

--- MultiFiles_MergerByCPP/mergerMain.cpp
+++ MultiFiles_MergerByCPP/mergerMain.cpp
@@ -3,13 +3,13 @@
 
 int main() {
 	MultiFilesMerger multiFilesMerger;
-	//vector<string> toMerge = { "MultiFiles_ToMerge_SmallFiles/00000001.txt", "MultiFiles_ToMerge_SmallFiles/00000002.txt" };
-	vector<string> toMerge;
-	char thisFileName[256];
-	for (int i = 1; i <= 1000000; i++) {
-		sprintf_s(thisFileName, "MultiFiles_ToMerge_SmallFiles/%08d.txt", i);
-		toMerge.push_back(thisFileName);
-	}
+	vector<string> toMerge = { "MultiFiles_ToMerge_SmallFiles/00000001.txt", "MultiFiles_ToMerge_SmallFiles/00000002.txt" };
+	//vector<string> toMerge;
+	//char thisFileName[256];
+	//for (int i = 1; i <= 1000000; i++) {
+	//	sprintf_s(thisFileName, "MultiFiles_ToMerge_SmallFiles/%08d.txt", i);
+	//	toMerge.push_back(thisFileName);
+	//}
 	multiFilesMerger.merge(toMerge);
 	return 0;
 }
